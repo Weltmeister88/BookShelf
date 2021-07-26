e5760ea2a9a04df76d22910982a79ebeabe4ec0c
zarko.lomic <zarko.lomic@fisglobal.com>
2021-07-26 23:22:29 +0200

    Add basic solution onion structure and setup Autofac conventions for services and repositories

---
+++ BookShelf.Infrastructure/Autofac/ConventionModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Autofac;
+
+namespace BookShelf.Infrastructure.Autofac
+{
+    public class ConventionModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Service", StringComparison.Ordinal))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Repository", StringComparison.Ordinal))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
